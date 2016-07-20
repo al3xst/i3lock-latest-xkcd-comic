b9cbcd57c53c4eb5128aa4495605f97f0ad3b6ec
al3xst <al3xst@gmail.com>
2016-07-20 12:49:58 +0200

    initial commit

---
+++ latest_xkcd.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+#crontab set to 0,30  //every 30min download latest comic...
+
+import json
+import urllib
+
+url = 'http://dynamic.xkcd.com/api-0/jsonp/comic/'
+
+if( urllib.urlopen(url).getcode() != 200 ) :
+	exit()
+
+output = json.load(urllib.urlopen(url))
+
+urllib.urlretrieve(output['img'], "xkcd.png")
